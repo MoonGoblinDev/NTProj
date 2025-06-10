31fecf2d658f4399a0892b119d476c0d2caef044
MoonGoblin <bregas.sat1@gmail.com>
2025-06-10 18:39:13 +0800

    Initial Commit

---
+++ Novel Translator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Novel Translator
+//
+//  Created by Bregas Satria Wicaksono on 10/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Novel Translator/Novel_TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Novel_TranslatorApp.swift
+//  Novel Translator
+//
+//  Created by Bregas Satria Wicaksono on 10/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Novel_TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
